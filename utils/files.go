c00f074ff8e9627e70bd7d7efdefb80d7e58ca38
Ben McHone <ben@mchone.dev>
2023-11-09 02:27:31 +0000

    Simplified read function

--- utils/files.go
+++ utils/files.go
@@ -1,27 +1,15 @@
 package utils
 
 import (
-	"bufio"
 	"os"
-	"strings"
 )
 
 func ReadFile(path string) string {
-	file, err := os.Open(path)
+	bytes, err := os.ReadFile(path)
 
 	if err != nil {
 		return ""
 	}
 
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	var arr []string
-
-	for scanner.Scan() {
-		arr = append(arr, scanner.Text())
-	}
-
-	return strings.Trim(strings.Join(arr, "\n"), "\n")
+	return string(bytes)
 }
